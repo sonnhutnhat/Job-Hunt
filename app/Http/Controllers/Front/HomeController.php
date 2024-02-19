9fa14d86431eabbf1d72d9d1f14bcea57b94aea4
sonnhutnhat <nguyenkienson3010@gmail.com>
2024-02-19 15:16:52 +0700

    upđate

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\PageHomeItem;
 
 class HomeController extends Controller
 {
     public function index() 
     {
-        return view('front.home');
+        $home_page_data = PageHomeItem::where('id',1)->first();
+        return view('front.home', compact('home_page_data'));
     }
 }
